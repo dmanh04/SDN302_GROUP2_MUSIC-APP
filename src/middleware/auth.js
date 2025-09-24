ccd2af20ec5c26eb2ce8baa8462f799ae40fdc8f
leminhduc306 <leminhducqvbn@gmail.com>
2025-09-24 21:50:18 +0700

    auth

---
+++ src/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+module.exports = function (req, res, next) {
+    const token = req.header('x-auth-token');
+
+    if (!token) {
+        return res.status(401).json({ msg: 'Không có token, quyền truy cập bị từ chối' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, 'your_jwt_secret');
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ msg: 'Token không hợp lệ' });
+    }
+};
