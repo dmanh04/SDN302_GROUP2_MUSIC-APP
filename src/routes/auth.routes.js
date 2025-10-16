094d7949ce46994965400c4dbc8242de592175aa
lenhatminh <lenhatminh2706@gmail.com>
2025-10-16 14:37:32 +0700

    fix route

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const router = express.Router()
+
+const authController = require("../controller/auth.controller")
+
+router.post("/sign-in", authController.signIn)
+
+router.post("/sign-up", authController.signUp)
+
+module.exports = router
